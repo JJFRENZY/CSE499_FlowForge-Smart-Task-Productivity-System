12ffdbf5059db5c2b29d6227d35dceb632d02050
JJFRENZY <jackjayeck@icloud.com>
2026-05-18 21:47:16 -0600

    week03.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-console.log("FlowForge Loaded Successfully");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.classList.add("task-item");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+});
