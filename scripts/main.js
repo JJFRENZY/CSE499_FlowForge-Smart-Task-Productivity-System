c3a789c9f442061d6980795cd34b2c04e2e7f622
JJFRENZY <jackjayeck@icloud.com>
2026-05-18 21:49:32 -0600

    week03.4

--- scripts/main.js
+++ scripts/main.js
@@ -11,14 +11,39 @@ addTaskBtn.addEventListener("click", function () {
         return;
     }
 
+    // Create task item
     const li = document.createElement("li");
 
     li.classList.add("task-item");
 
-    li.textContent = taskText;
+    // Create task text
+    const span = document.createElement("span");
 
+    span.textContent = taskText;
+
+    // Create delete button
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.classList.add("delete-btn");
+
+    // Delete functionality
+    deleteBtn.addEventListener("click", function () {
+
+        li.remove();
+
+    });
+
+    // Add text + button into task item
+    li.appendChild(span);
+
+    li.appendChild(deleteBtn);
+
+    // Add task item to page
     taskList.appendChild(li);
 
+    // Clear input
     taskInput.value = "";
 
 });
