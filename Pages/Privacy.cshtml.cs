a4e4f8489f33fa3bce4070c60f2ee52964162b00
JJFRENZY <jackjayeck@icloud.com>
2026-05-19 18:57:02 -0600

    week03.7

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSE499_FlowForge_Smart_Task_Productivity_System.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
